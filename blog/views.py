689e531ae60c28667d4d835cfd750ce2e6c9157d
sjohansso-n <46817969+sjohansso-n@users.noreply.github.com>
2019-01-19 13:51:47 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Tag
 from itertools import chain
